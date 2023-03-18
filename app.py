b8b5d8ae737f24a9927d1dcda06d5b67208d3814
MohdMustakimAnsari <mustakimmohd603@gmail.com>
2023-03-18 12:42:56 +0530

    added about api

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get('/about')
+def about(name:str):
+    return{f'your name is :{name}'}
+
+
 if __name__=='__main__':
     uvicorn.run(app)
